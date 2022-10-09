2794da82a14a58bf2d2a262d63af71ccfd8b1717
Орлов Сергей <orlovsergey263759223@gmail.com>
2022-10-09 20:33:04 +0300

    версия 4

--- yatube/posts/tests/test_routes.py
+++ yatube/posts/tests/test_routes.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
 from django.urls import reverse
 
+from urllib.parse import urljoin
+
 
 GROUP_SLUG = 'test-slug'
 
@@ -16,91 +18,40 @@ URL_GROUP_LIST_PAGE = f'/group/{GROUP_SLUG}/'
 
 URL_FOLLOW_INDEX_PAGE = '/follow/'
 
-URL_FOLLOW_PAGE = f'/profile/{USER_USERNAME}/follow/'
+URL_FOLLOW_PAGE = urljoin(URL_PROFILE_PAGE, 'follow/')
 
-URL_UNFOLLOW_PAGE = f'/profile/{USER_USERNAME}/unfollow/'
+URL_UNFOLLOW_PAGE = urljoin(URL_PROFILE_PAGE, 'unfollow/')
 
 POST_ID = 1
 
 URL_POST_DETAIL_PAGE = f'/posts/{POST_ID}/'
 
-URL_POST_EDIT_PAGE = f'/posts/{POST_ID}/edit/'
+URL_POST_EDIT_PAGE = urljoin(URL_POST_DETAIL_PAGE, 'edit/')
 
-URL_ADD_COMMENT_PAGE = f'/posts/{POST_ID}/comment/'
+URL_ADD_COMMENT_PAGE = urljoin(URL_POST_DETAIL_PAGE, 'comment/')
 
 
 class PostsURLTests(TestCase):
 
     def test_1(self):
         links = {
-            reverse('posts:index'): URL_MAIN_PAGE,
-            reverse(
-                'posts:group_list',
-                kwargs={'slug': GROUP_SLUG}
-            ): URL_GROUP_LIST_PAGE,
-            reverse(
-                'posts:profile',
-                kwargs={'username': USER_USERNAME}
-            ): URL_PROFILE_PAGE,
-            reverse(
-                'posts:post_detail',
-                kwargs={'post_id': POST_ID}
-            ): URL_POST_DETAIL_PAGE,
-            reverse('posts:post_create'): URL_POST_CREATE_PAGE,
-            reverse(
-                'posts:post_edit',
-                kwargs={'post_id': POST_ID}
-            ): URL_POST_EDIT_PAGE,
-            reverse(
-                'posts:add_comment', kwargs={'post_id': POST_ID}
-            ): URL_ADD_COMMENT_PAGE,
-            reverse('posts:follow_index'): URL_FOLLOW_INDEX_PAGE,
-            reverse(
-                'posts:profile_follow',
-                kwargs={'username': USER_USERNAME}
-            ): URL_FOLLOW_PAGE,
-            reverse(
-                'posts:profile_unfollow',
-                kwargs={'username': USER_USERNAME}
-            ): URL_UNFOLLOW_PAGE
+            'posts:index': (URL_MAIN_PAGE, None),
+            'posts:group_list': (URL_GROUP_LIST_PAGE, {'slug': GROUP_SLUG}),
+            'posts:profile': (URL_PROFILE_PAGE, {'username': USER_USERNAME}),
+            'posts:post_detail': (URL_POST_DETAIL_PAGE, {'post_id': POST_ID}),
+            'posts:post_create': (URL_POST_CREATE_PAGE, None),
+            'posts:post_edit': (URL_POST_EDIT_PAGE, {'post_id': POST_ID}),
+            'posts:add_comment': (URL_ADD_COMMENT_PAGE, {'post_id': POST_ID}),
+            'posts:follow_index': (URL_FOLLOW_INDEX_PAGE, None),
+            'posts:profile_follow':
+            (URL_FOLLOW_PAGE, {'username': USER_USERNAME}),
+            'posts:profile_unfollow':
+            (URL_UNFOLLOW_PAGE, {'username': USER_USERNAME}),
         }
-        for url, value in links.items():
-            with self.subTest(url=url):
-                self.assertEqual(url, value)
-
-    #def test_routes(self):
-    #    links = {
-    #        reverse('posts:index'): URL_MAIN_PAGE,
-    #        reverse(
-    #            'posts:group_list',
-    #            kwargs={'slug': GROUP_SLUG}
-    #        ): URL_GROUP_LIST_PAGE,
-    #        reverse(
-    #            'posts:profile',
-    #            kwargs={'username': USER_USERNAME}
-    #        ): URL_PROFILE_PAGE,
-    #        reverse(
-    #            'posts:post_detail',
-    #            kwargs={'post_id': POST_ID}
-    #        ): URL_POST_DETAIL_PAGE,
-    #        reverse('posts:post_create'): URL_POST_CREATE_PAGE,
-    #        reverse(
-    #            'posts:post_edit',
-    #            kwargs={'post_id': POST_ID}
-    #        ): URL_POST_EDIT_PAGE,
-    #        reverse(
-    #            'posts:add_comment', kwargs={'post_id': POST_ID}
-    #        ): URL_ADD_COMMENT_PAGE,
-    #        reverse('posts:follow_index'): URL_FOLLOW_INDEX_PAGE,
-    #        reverse(
-    #            'posts:profile_follow',
-    #            kwargs={'username': USER_USERNAME}
-    #        ): URL_FOLLOW_PAGE,
-    #        reverse(
-    #            'posts:profile_unfollow',
-    #            kwargs={'username': USER_USERNAME}
-    #        ): URL_UNFOLLOW_PAGE
-    #    }
-    #    for url, value in links.items():
-    #        with self.subTest(url=url):
-    #            self.assertEqual(url, value)
+        for key_rev, value_tuple in links.items():
+            expected_link = value_tuple[0]
+            kwargs_dct = value_tuple[1]
+            with self.subTest(key_rev=key_rev):
+                self.assertEqual(
+                    reverse(key_rev, kwargs=kwargs_dct), expected_link
+                )
